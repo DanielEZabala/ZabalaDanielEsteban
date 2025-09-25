a7ec783e5304a962f337f5ac6447bb7eec6e25a1
DanielEZabala <danielzzabala@gmail.com>
2025-09-25 10:59:10 -0300

    Update listar.php

--- Proyecto2025/listar.php
+++ Proyecto2025/listar.php
@@ -1,16 +1,1 @@
-<?php
 
-
-require_once "User.php";
-
-$user = new User();
-$users = $user->getAll();
-foreach ($users as $u) {
- echo $u['id'] . " - " . $u['nombre'] . " " . $u['apellido'] . "<br>";
-}
-
-$user -> getAll();
-
-
-
-?>
