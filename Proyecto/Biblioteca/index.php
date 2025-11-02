f0be45850de403ddf69ea1708bde7006a890a628
DanielEZabala <danielzzabala@gmail.com>
2025-11-02 01:49:36 -0300

    Delete Proyecto/Biblioteca directory

--- Proyecto/Biblioteca/index.php
+++
@@ -1,8 +0,0 @@
-<?php
- echo "<br><a href='usuarios/formularioUsuario.php'>Registrar Usuario</a>";
- echo "<br><a href='libro/formularioLibro.php'>Registrar Libro</a>";
-  echo "<br><a href='prestamos/solicitudPrestamo.php'>Solicitar Prestamo de Libro</a>";
-
-
-
-?>
